f2608f4bf5fc996a2f670fbefe86795975b57628
Md-Tharik <mdtharik563@gmail.com>
2026-02-11 01:19:40 +0530

    Change Razorpay key to test environment

--- config.js
+++ config.js
@@ -12,4 +12,4 @@ const firebaseConfig = {
 const ADMIN_EMAIL = "tharikm421azaz@gmail.com"; 
 
 // Razorpay Key (Optional - Leave as is for testing)
-const RAZORPAY_KEY = "rzp_live_SEWToMaVY9wv59"; 
+const RAZORPAY_KEY = "rzp_test_SEZ1tQeGbrCpRv"; 
